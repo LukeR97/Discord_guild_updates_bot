c5e647ab51eb3f67e0a46553c5b5c69a41e7df84
Luke <rothwell.luke@gmail.com>
2023-10-26 18:32:43 +0100

    deploy package

--- main.tf
+++ main.tf
@@ -7,4 +7,12 @@ resource "hcloud_server" "discord-guild-updates" {
   image = "ubuntu-20.04"
   server_type  = "cx11"
   datacenter = "nbg1-dc3"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+output "server_ip" {
+  value       = "hcloud_server.discord-guild-updates.ipv_address"
 }
