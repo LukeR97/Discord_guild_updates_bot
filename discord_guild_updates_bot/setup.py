e92cf98142d3c98c2272b5286593d210feea5013
Luke <rothwell.luke@gmail.com>
2023-10-27 16:22:56 +0100

    add requirements.txt

--- discord_guild_updates_bot/setup.py
+++ discord_guild_updates_bot/setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='discord-guild-updates-bot',
     version='1.0',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
             'discord-guild-updates = discord_guild_updates_bot.main:main',
